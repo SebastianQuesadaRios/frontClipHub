83a358dc67ed39b098790d89a757754a74b40883
sebastian quesada <sebastianquesadarios@gmail.com>
2024-11-14 21:24:37 -0500

    error en el app.jsx 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Forms';
+import Forms from './components/Forms';
 import UploadVideo from './components/UploadVideo';
 import Home from './components/Home';  // Si tienes un Home donde se muestran los videos
 
@@ -7,7 +7,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Forms />} />
                 <Route path="/upload-video" element={<UploadVideo />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
